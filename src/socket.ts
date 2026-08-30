b3e75d7a35fc07bd882e2307eaa18d6d9c6a61b4
Prashant Gupta <prashantguptaofficial007@gmail.com>
2026-08-30 17:43:05 +0530

    setup websocket server

--- src/socket.ts
+++ src/socket.ts
@@ -1,29 +1,21 @@
 import { createServer } from "node:http";
-import config from 'config'
-import { Server } from "socket.io";
+import { Server } from 'socket.io';
 
-const wsServer = createServer();
+const wsServer = createServer()
 
-// todo: move origin value to the config.
-const ALLOWED_DOMAINS = [
-  config.get("frontend.clientUI"),
-  config.get("frontend.adminUI"),
-];
+const io = new Server(wsServer, {cors:{origin: "http://localhost:5173"}})
 
+io.on("connection", (socket)=>{
+  console.log("Client connected", socket.id)
 
-const io = new Server(wsServer, { cors: { origin:  ALLOWED_DOMAINS as string[]} });
+  socket.on("join", (data)=>{
+    socket.join(String(data.tenantId))
 
-io.on("connection", (socket) => {
-  console.log("Client connected", socket.id);
-
-  socket.on("join", (data) => {
-    socket.join(String(data.tenantId));
-
-    socket.emit("join", { roomId: String(data.tenantId) });
-  });
-});
+    socket.emit("join", {roomId: String(data.tenantId)})
+  })
+})
 
 export default {
   wsServer,
-  io,
-};
+  io
+}

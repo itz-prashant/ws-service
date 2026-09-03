415c76f2e1c686d9bb0f6e9347affe82a70d1c8c
Prashant Gupta <prashantguptaofficial007@gmail.com>
2026-09-03 19:45:42 +0530

    improve cors

--- src/socket.ts
+++ src/socket.ts
@@ -1,23 +1,32 @@
+import config from "config";
 import { createServer } from "node:http";
-import { Server } from 'socket.io';
+import { Server } from "socket.io";
 
-const wsServer = createServer()
+const wsServer = createServer();
 
-const io = new Server(wsServer, {cors:{origin: "http://localhost:5173"}})
+const io = new Server(wsServer, {
+  cors: {
+    origin: [
+      config.get("frontend.ADMIN_DASHBOARD_BASE_URL"),
+      config.get("frontend.clientUI"),
+      config.get("frontend.adminUI"),
+    ],
+  },
+});
 
-io.on("connection", (socket)=>{
-  console.log("Client connected", socket.id)
+io.on("connection", (socket) => {
+  console.log("Client connected", socket.id);
 
-  socket.on("join", (data)=>{
-    socket.join(String(data.tenantId)) // join rooms
+  socket.on("join", (data) => {
+    socket.join(String(data.tenantId)); // join rooms
 
     // Get rooms
     // console.log(io.of("/").adapter.rooms)
-    socket.emit("join", {roomId: String(data.tenantId)})
-  })
-})
+    socket.emit("join", { roomId: String(data.tenantId) });
+  });
+});
 
 export default {
   wsServer,
-  io
-}
+  io,
+};
